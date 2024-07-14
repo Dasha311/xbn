70f992f379418f251fce857d558bca5aa17f21a9
Dasha311 <151998213+Dasha311@users.noreply.github.com>
2024-07-14 22:49:47 +0500

    l
    l

---
+++ scripts.js
@@ -0,0 +1,87 @@
+const translations = {
+    ru: {
+        about: 'О туре',
+        tours: 'Расписание туров',
+        booking: 'Бронирование',
+        location: 'Как нас найти',
+        aboutTitle: 'О ТУРЕ',
+        aboutText: 'Погрузитесь в первобытную казахскую степь, живите в юртах и готовьте еду на костре. Испытайте настоящую жизнь кочевников! Мы обещаем только незабываемые эмоции.',
+        toursTitle: 'РАСПИСАНИЕ ТУРОВ',
+        tour1: 'Великолепная экскурсия на Кайракский водопад',
+        tour1Desc: 'Продолжительность: 1 день. Описание тура: Мы предлагаем Вам, качественную и доступную экскурсию на Кайракский водопад. Вы сможете хорошо провести время с семьей и друзьями, а мы Вам в этом поможем. Также мы можем организовать индивидуальную экскурсию на машине в будние дни.',
+        tour3: 'Кольсай, Кайынды + 3 каньона',
+        tour3Desc: 'Продолжительность: 2 дня. Описание тура: Возможно ли посетить главные достопримечательности Алматинской области за 2 дня? Без сомнений — да! За время тура Вы посетите сразу несколько чудес Казахстана: озера Кольсай, Кайынды и каньоны реки Чарын (Долину замков, Куртогай, Черный каньон). На протяжении всего маршрута Вас будет сопровождать опытный гид, который поделится интересной информацией о посещаемых объектах и точно не даст скучать в пути.',
+        tour5: 'Экскурсия в город Жаркент',
+        tour5Desc: 'Продолжительность: 1 день. Описание тура: Город Жаркент расположен в 320 км от от Алматы по трассе Сарыозек-Хоргос, в 30 км от границы Казахстана с Китаем. Через данный город еще в Х веке проходила одна из ветвей Великого Шелкового пути. Ученый и путешественник Ч. Валиханов писал о Жаркенте в своих дневниках.',
+        bookingTitle: 'БРОНИРОВАНИЕ',
+        labelName: 'Имя:',
+        labelEmail: 'Email:',
+        labelTour: 'Выберите тур:',
+        submit: 'Отправить заявку'
+    },
+    en: {
+        about: 'About',
+        tours: 'Tour Schedule',
+        booking: 'Booking',
+        location: 'Location',
+        aboutTitle: 'ABOUT THE TOUR',
+        aboutText: 'Immerse yourself in the primeval Kazakh steppe, live in yurts, and cook on an open fire. Experience the true life of nomads! We promise unforgettable emotions.',
+        toursTitle: 'TOUR SCHEDULE',
+        tour1: 'Magnificent tour to Kairak waterfall',
+        tour1Desc: 'Duration: 1 day. Tour description: We offer you a quality and affordable tour to Kairak Waterfall. You can spend quality time with your family and friends, and we will assist you with that. We can also arrange an individual car tour on weekdays.',
+        tour3: 'Kolsay, Kaindy + 3 canyons',
+        tour3Desc: 'Duration: 2 days. Tour description: Is it possible to visit the main attractions of the Almaty region in 2 days? Without a doubt — yes! During the tour, you will visit several wonders of Kazakhstan at once: Kolsai and Kaindy lakes, and the canyons of the Charyn River (Valley of the Castles, Kurtogai, Black Canyon). Throughout the route, you will be accompanied by an experienced guide who will share interesting information about the visited objects and will not let you get bored on the way.',
+        tour5: 'Excursion to the city of Zharkent',
+        tour5Desc: 'Duration: 1 day. Tour description: The city of Zharkeat is located 320 km from Almaty on the Saryozek-Khorgos highway, 30 km from the border of Kazakhstan with China. Through this city, one of the branches of the Great Silk Road passed back in the 19th century. The scientist and traveler Ch. Valihanov wrote about Zharkeat in his diaries.',
+        bookingTitle: 'BOOKING',
+        labelName: 'Name:',
+        labelEmail: 'Email:',
+        labelTour: 'Select tour:',
+        submit: 'Submit'
+    },
+    kz: {
+        about: 'Тур туралы',
+        tours: 'Тур кестесі',
+        booking: 'Брондау',
+        location: 'Бізді табу',
+        aboutTitle: 'ТУР ТУРАЛЫ',
+        aboutText: 'Қазақ даласының алғашқы өміріне еніңіз, киіз үйлерде тұрып, ашық отта тамақ пісіріңіз. Көшпенділердің шынайы өмірін сезініңіз! Біз ұмытылмас эмоцияларды уәде етеміз.',
+        toursTitle: 'ТУР КЕСТЕСІ',
+        tour1: 'Қайрақ сарқырамасына керемет экскурсия',
+        tour1Desc: 'Уақытша: 1 күн. Тур сипаттамасы: Біз сізге Кайрак шүлденін өнімді және қол жетімді тур ұсынамыз. Сіз отбасы және досқа үшін жақсы уақыт өтіре аласыз, бірақ біз сізге осында көмек етеміз. Осыдан тыс күндерде күнде сыртта қызмет көрсете аламыз.',
+        tour3: 'Көлсай, Қайыңды + 3 каньон',
+        tour3Desc: 'Уақытша: 2 күн. Тур сипаттамасы: Алматы облысының ең басты көріністерін 2 күнде тікелейді мінеу құрметті жерлеріне өздері. Осылайшағын, құнындағы әлемдің барлық ақша желісі: көлсай және қайыңды. көліктік іске. сұраушылығын қызмет қызметіндегілеуші құндыздық жүрісті ұсынатын әділ болады.',
+        tour5: 'Жаркент қаласына экскурсия',
+        tour5Desc: 'Уақытша: 1 күн. Тур сипаттамасы: Жаркент қаласы Алматы қаласының 320 км нөкеріндегі Сарыозек-Хоргос шосесінде жай орналасқан, Қазақстан мен Қытай арасындағы 30 км орналасқан қаласы. Жаңа кезекте Бекіті Шелкови алдында 19-нжы ғасырда тым жиырма тұр. Ғазырлық ертеңіндегі Ж. Валиханов әкімі және мансур айтулы әбілетті, дәулетті жасау құлағында өмір сүріп жатқанын ұстады.',
+        bookingTitle: 'БРОНДАУ',
+        labelName: 'Аты:',
+        labelEmail: 'Email:',
+        labelTour: 'Турды таңдаңыз:',
+        submit: 'Өтінім жіберу'
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const langButtons = document.querySelectorAll('.language-switcher button');
+    langButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const lang = button.getAttribute('data-lang');
+            setLanguage(lang);
+            langButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        });
+    });
+
+    function setLanguage(lang) {
+        document.querySelectorAll('[data-lang-key]').forEach(element => {
+            const key = element.getAttribute('data-lang-key');
+            if (translations[lang][key]) {
+                element.textContent = translations[lang][key];
+            }
+        });
+    }
+
+    // Set default language
+    setLanguage('ru');
+});
+
